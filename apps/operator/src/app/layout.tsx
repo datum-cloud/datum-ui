6c032a54e6611eefa022838db81224e8ec0a7a88
Jordan Springer <jordan@showmewebs.com>
2023-12-14 11:42:16 -0600

    theme setups (#8)
    * theme setups
    
    * no double exports

--- apps/operator/src/app/layout.tsx
+++ apps/operator/src/app/layout.tsx
@@ -1,23 +1,23 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import '@repo/ui/styles.css'
 
-import "@repo/ui/styles.css";
-
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Operator Portal | Datum | Start here, go anywhere",
-  description: "The open source foundation of a sustainable digital world",
-};
+  title: 'Operator Portal | Datum | Start here, go anywhere',
+  description: 'The open source foundation of a sustainable digital world',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }): JSX.Element {
   return (
-    <html lang="en">
+    <html className="h-full relative" lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
--- apps/operator/src/app/page.tsx
+++ apps/operator/src/app/page.tsx
@@ -1,9 +1,23 @@
-import { Button } from "@repo/ui/button";
+import React from 'react'
+import Image from 'next/image'
+import { Button } from '@repo/ui/button'
+import logoReversed from '../../public/logos/full_reversed.svg'
 
-export default function Page(): JSX.Element {
+const Landing: React.FC = () => {
   return (
-    <main className="flex flex-col min-h-screen items-center space-between p-24 bg-blue-600">
-      <Button appName="Operator Portal">Click Me!</Button>
+    <main className="flex flex-col min-h-screen w-full items-center space-between sp-24 dark:bg-dk-surface-0 bg-surface-0">
+      <div className="flex flex-col justify-center mx-auto my-auto w-full p-6 sm:w-1/3 h-full relativee ease-in-out">
+        <Image
+          alt="datum imagery background"
+          className="mx-auto"
+          priority
+          src={logoReversed as string}
+          width={385}
+        />
+        <Button appName="Datum Operator Portal">Let&apos;s get started</Button>
+      </div>
     </main>
-  );
+  )
 }
+
+export default Landing
