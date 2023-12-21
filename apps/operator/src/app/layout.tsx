d867480ab14e63394afb7e55b6a8c03572e7acfa
springerkc <jordan@showmewebs.com>
2023-12-21 10:37:33 -0600

    fix: import order

--- apps/operator/src/app/layout.tsx
+++ apps/operator/src/app/layout.tsx
@@ -1,7 +1,8 @@
-import './globals.css'
-import '@repo/ui/styles.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+
+import './globals.css'
+import '@repo/ui/styles.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
