47d7a6a7c139e59fe8ec72b72172a9982c528f94
Jordan Springer <jordan@showmewebs.com>
2024-01-09 13:08:16 -0600

    fix: style overrides (#19)
    * i actually hate CSS and styles fwiw
    
    * nested comps use important :face_palming:
    
    * :broom:
    
    * ughh

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from "tailwindcss";
-import sharedConfig from "@repo/tailwind-config";
+import type { Config } from 'tailwindcss'
+import sharedConfig from '@repo/tailwind-config'
 
-const config: Pick<Config, "prefix" | "content" | "presets"> = {
-  content: ["./src/**/*.tsx"],
-  prefix: "ui-",
+const config: Pick<Config, 'prefix' | 'content' | 'presets'> = {
+  content: ['./src/**/*.tsx'],
+  prefix: 'ui-',
   presets: [sharedConfig],
-};
+}
 
-export default config;
+export default config
