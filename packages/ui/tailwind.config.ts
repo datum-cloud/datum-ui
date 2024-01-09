912fa703f189cbf4a474827cd670c0c585a8c833
springerkc <jordan@showmewebs.com>
2024-01-09 10:35:38 -0600

    testing

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,9 +1,9 @@
-import type { Config } from "tailwindcss";
-import sharedConfig from "@repo/tailwind-config";
+import type { Config } from 'tailwindcss';
+import sharedConfig from '@repo/tailwind-config';
 
-const config: Pick<Config, "prefix" | "content" | "presets"> = {
-  content: ["./src/**/*.tsx"],
-  prefix: "ui-",
+const config: Pick<Config, 'prefix' | 'content' | 'presets'> = {
+  content: ['./src/**/*.tsx'],
+  prefix: 'ui-',
   presets: [sharedConfig],
 };
 
