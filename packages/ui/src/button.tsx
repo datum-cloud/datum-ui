ac49254f5dc1d46ae982000937bc004a6992afc8
springerkc <jordan@showmewebs.com>
2024-01-09 12:20:17 -0600

    nested comps use important :face_palming:

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -10,7 +10,7 @@ export const Button = React.forwardRef<HTMLButtonElement, any>((props, ref) => {
       {...rest}
       type={rest.type ? rest.type : 'submit'}
       className={clsx(
-        `ui-px-4 ui-py-3 ui-bg-sunglow-900 ui-rounded-md`,
+        `!ui-px-4 !ui-py-3 !ui-bg-sunglow-900 !ui-rounded-md`,
         className,
       )}
       ref={ref}
--- packages/ui/src/text-input.tsx
+++ packages/ui/src/text-input.tsx
@@ -8,7 +8,7 @@ export const TextInput = React.forwardRef<HTMLInputElement, any>(
     return (
       <input
         className={clsx(
-          `ui-w-full ui-h-10 ui-rounded-md ui-pl-2 ui-ring-blackberry-800 dark:ui-peat-800 focus:outline-0 dark:ui-text-white ui-text-blackberry-800 dark:ui-bg-peat-900 ui-bg-blackberry-50 border ui-border-blackberry-200 dark:ui-border-peat-700`,
+          `!ui-w-full !ui-h-10 !ui-rounded-md !ui-pl-2 !ui-ring-blackberry-800 dark:!ui-peat-800 focus:!outline-0 dark:!ui-text-white !ui-text-blackberry-800 dark:!ui-bg-peat-900 !ui-bg-blackberry-50 border !ui-border-blackberry-200 dark:!ui-border-peat-700`,
           props.className,
         )}
         ref={ref}
