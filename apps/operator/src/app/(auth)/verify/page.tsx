771aa57b4f9b087fe0db78055c789421ed6548ef
springerkc <jordan@showmewebs.com>
2024-01-10 09:40:31 -0600

    review changes 00

--- apps/operator/src/app/(auth)/verify/page.tsx
+++ apps/operator/src/app/(auth)/verify/page.tsx
@@ -2,10 +2,16 @@
 
 import React from 'react'
 import Image from 'next/image'
+import { useSearchParams } from 'next/navigation'
+import { useVerifyUser } from '../../../lib/user'
 import { Button } from '@repo/ui/button'
 import logoReversed from '../../../../public/logos/logo_orange_icon.svg'
 
 const VerifyUser: React.FC = () => {
+  const token = useSearchParams().get('token')
+
+  const { isLoading, error } = useVerifyUser(token ? token : null)
+
   return (
     <main className="flex flex-col min-h-screen w-full items-center space-between dark:bg-dk-surface-0 bg-surface-0">
       <div className="flex flex-col justify-center mx-auto my-auto w-full p-6 sm:w-1/3 h-full relative ease-in-out">
