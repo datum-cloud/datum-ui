c4065a60b980695096612cb1e1a57a86673be50b
ahussein3 <ahussein@outlook.com>
2024-03-18 22:48:32 +0200

    refactor social icons to it's own folder

--- apps/operator/src/app/(auth)/login/page.tsx
+++ apps/operator/src/app/(auth)/login/page.tsx
@@ -14,6 +14,8 @@ import { type LoginUser } from '../../../lib/user'
 import visibilityOff from '../../../../public/icons/visibility-off.svg'
 import visibility from '../../../../public/icons/visibility.svg'
 import LandingBackground from '../../../../public/backgrounds/landing-bg.svg'
+import googleIcon from '../../../../public/icons/google-icon.svg'
+import githubIcon from '../../../../public/icons/github-icon.svg'
 
 const AuthLogin: React.FC = () => {
   const router = useRouter()
@@ -112,26 +114,18 @@ const AuthLogin: React.FC = () => {
               </div>
 
               <div className="w-full">
-              <button
+                <button
                   className="!text-blackberry-900 w-full bg-white hover:bg-blackberry-50 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium border border-winter-sky-900 rounded-lg text-sm px-5 py-2.5 text-center inline-flex justify-center me-1 mb-2 ml-1"
                   onClick={() => {
                     google()
                   }}
                   type="button"
                 >
-                  <svg
-                    aria-hidden="true"
-                    className="w-4 h-4 me-2"
-                    fill="currentColor"
-                    viewBox="0 0 18 19"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      clipRule="evenodd"
-                      d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z"
-                      fillRule="evenodd"
-                    />
-                  </svg>
+                  <Image
+                    alt="google icon"
+                    src={googleIcon}
+                    className="w-4 h-4 mr-4 mt-0.5"
+                  />
                   Sign in with Google
                 </button>
                 <button
@@ -141,23 +135,13 @@ const AuthLogin: React.FC = () => {
                   }}
                   type="button"
                 >
-                  <svg
-                    aria-hidden="true"
-                    className="w-4 h-4 me-2"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      clipRule="evenodd"
-                      d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
-                      fillRule="evenodd"
-                    />
-                  </svg>
+                  <Image
+                    alt="github icon"
+                    src={githubIcon}
+                    className="w-4 h-4 mr-4 mt-0.5"
+                  />
                   Sign in with Github
                 </button>
-
-
               </div>
 
               <Button
