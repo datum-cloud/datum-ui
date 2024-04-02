e87755844a023a4020b41a6a62c08bbbcf0ba064
hannahdatum <hannahking@datum.net>
2024-04-02 11:34:39 +0100

    fix warnings
    Signed-off-by: hannahdatum <hannahking@datum.net>

--- apps/operator/src/components/page-title.tsx
+++ apps/operator/src/components/page-title.tsx
@@ -1,13 +1,9 @@
 const PageTitle = ({ title, description }: any) => {
   return (
-    <h1
-      className={`text-4xl leading-10 mb-4 text-left w-full font-bold font-mono`}
-    >
+    <h1 className="text-4xl leading-10 mb-4 text-left w-full font-bold font-mono">
       {title}
       {description ? (
-        <p
-          className={`text-2xl mt-4 leading-5 text-left font-thin font-sans`}
-        >
+        <p className="text-2xl mt-4 leading-5 text-left font-thin font-sans">
           {description}
         </p>
       ) : null}
