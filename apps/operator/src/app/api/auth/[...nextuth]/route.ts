f523f3feed081420c010bee18b9e8d2dae1d0e01
hannahdatum <hannahking@datum.net>
2024-04-02 23:33:48 +0100

    fix typescript errors
    Signed-off-by: hannahdatum <hannahking@datum.net>

--- apps/operator/src/app/api/auth/[...nextuth]/route.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Export the route handlers from our auth confi
- * and set the runtime to the Edge
- */
-export { GET, POST } from '../../../../lib/auth'
-export const runtime = 'edge'
--- packages/config-eslint/next.js
+++ packages/config-eslint/next.js
@@ -1,55 +1,57 @@
-const { resolve } = require("node:path");
+const { resolve } = require('node:path')
 
-const project = resolve(process.cwd(), "tsconfig.json");
+const project = resolve(process.cwd(), 'tsconfig.json')
 
 module.exports = {
-	extends: [
-		"@vercel/style-guide/eslint/node",
-		"@vercel/style-guide/eslint/typescript",
-		"@vercel/style-guide/eslint/browser",
-		"@vercel/style-guide/eslint/react",
-		"@vercel/style-guide/eslint/next",
-		"eslint-config-turbo",
-	].map(require.resolve),
-	plugins: [
-		"eslint-plugin-tsdoc",
-	],
-	parserOptions: {
-		project,
-	},
-	globals: {
-		React: true,
-		JSX: true,
-	},
-	settings: {
-		"import/resolver": {
-			typescript: {
-				project,
-			},
-			node: {
-				extensions: [".mjs", ".js", ".jsx", ".ts", ".tsx"],
-			},
-		},
-	},
-	ignorePatterns: ["node_modules/", "dist/"],
-	// add rules configurations here
-	rules: {
-		"import/no-default-export": 0,
-		"no-console": 1,
-		"no-unsafe-unassignment": 0,
-		"tsdoc/syntax": 0,
-		"react/function-component-definition": ["off", {
-			namedComponents: "arrow-function",
-			unnamedComponents: "arrow-function",
-		}],
-		"@typescript-eslint/no-unsafe-assignment": 0,
-		"@typescript-eslint/no-unsafe-unassignment": 0,
-		"@typescript-eslint/no-unsafe-member-access": 0,
-		"@typescript-eslint/no-unsafe-return": 0,
-		"@typescript-eslint/no-unsafe-call": 0,
-		"@typescript-eslint/no-unsafe-argument": 0,
-		"@typescript-eslint/no-explicit-any": 0,
-		"@typescript-eslint/no-floating-promises": 0,
-		"@typescript-eslint/explicit-function-return-type": 0
-	},
-};
+  extends: [
+    '@vercel/style-guide/eslint/node',
+    '@vercel/style-guide/eslint/typescript',
+    '@vercel/style-guide/eslint/browser',
+    '@vercel/style-guide/eslint/react',
+    '@vercel/style-guide/eslint/next',
+    'eslint-config-turbo',
+  ].map(require.resolve),
+  plugins: ['eslint-plugin-tsdoc'],
+  parserOptions: {
+    project,
+  },
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+      node: {
+        extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  ignorePatterns: ['node_modules/', 'dist/'],
+  // add rules configurations here
+  rules: {
+    'import/no-default-export': 0,
+    'no-console': 1,
+    'no-unsafe-unassignment': 0,
+    'tsdoc/syntax': 0,
+    'react/function-component-definition': [
+      'off',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    '@typescript-eslint/no-unsafe-assignment': 0,
+    '@typescript-eslint/no-unsafe-unassignment': 0,
+    '@typescript-eslint/no-unsafe-member-access': 0,
+    '@typescript-eslint/no-unsafe-return': 0,
+    '@typescript-eslint/no-unsafe-call': 0,
+    '@typescript-eslint/no-unsafe-argument': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-floating-promises': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/restrict-template-expressions': 0,
+  },
+}
