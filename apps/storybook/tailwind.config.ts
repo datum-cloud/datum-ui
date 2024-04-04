24c2f6fa9824077c100d512c7e3e68212ee80ec9
ahussein3 <ahussein@outlook.com>
2024-04-04 21:41:42 +0200

    Reference colorPalette from tailwindConfig to storybook

--- apps/storybook/tailwind.config.ts
+++ apps/storybook/tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "tailwindcss"
+import sharedConfig from "@repo/tailwind-config"
 
 const config = {
   content: [
     "./src/stories/**/*.{ts,tsx}",
     "../../packages/ui/components/**/*.{ts,tsx}",
   ],
+  presets: [sharedConfig],
 } satisfies Config
 
 export default config
