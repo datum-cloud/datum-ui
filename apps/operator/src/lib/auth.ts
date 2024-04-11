f3561b5e03a91d15f1f1741069079366211cd42c
Golang Lemonade <147884153+golanglemonade@users.noreply.github.com>
2024-04-10 21:27:39 -0600

    set session cookie on creds login
    Signed-off-by: Golang Lemonade <147884153+golanglemonade@users.noreply.github.com>

--- apps/operator/src/lib/auth.ts
+++ apps/operator/src/lib/auth.ts
@@ -3,7 +3,8 @@ import NextAuth from 'next-auth'
 import Credentials from 'next-auth/providers/credentials'
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
-import { restUrl } from '@repo/dally/auth'
+import { restUrl, sessionCookieName } from '@repo/dally/auth'
+import { cookies } from 'next/headers'
 
 export const config = {
   theme: {
@@ -31,6 +32,7 @@ export const config = {
       async authorize(credentials: any) {
         let accessToken = ''
         let refreshToken = ''
+        let session = ''
 
         // If the request did not come through with the access tokens, take the
         // credentials and use them to get the access token and refresh token
@@ -62,13 +64,20 @@ export const config = {
             // get access token and refresh tokens from response
             accessToken = data?.access_token
             refreshToken = data?.refresh_token
+
+            session = data?.session
           }
         } else {
           // else these came from the sign in call back and we can use them
           // to proceed
           accessToken = credentials.accessToken
           refreshToken = credentials.refreshToken
+          session = credentials.session
         }
+
+        // Set the session cookie for the user
+        console.log('Setting session cookie', `${sessionCookieName}`)
+        cookies().set(`${sessionCookieName}`, session)
 
         // Get user data for sessions
         const uData = await fetch(`${restUrl}/oauth/userinfo`, {
@@ -111,6 +120,7 @@ export const config = {
         // set access token to pull additional data from the API
         user.accessToken = data?.access_token
         user.refreshToken = data?.refresh_token
+        user.session = data?.session
 
         // Get user data for sessions
         const uData = await fetch(`${restUrl}/oauth/userinfo`, {
