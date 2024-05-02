43c00ebc4629d5b521a17ffbb89e416730e0788a
hannahdatum <163343744+hannahdatum@users.noreply.github.com>
2024-05-02 20:04:46 +0100

    sanity studio setup (#119)
    Signed-off-by: hannahdatum <hannahking@datum.net>

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ['@repo/dally', '@repo/ui', '@repo/codegen'],
+  experimental: {
+    // Used to guard against accidentally leaking SANITY_API_READ_TOKEN to the browser
+    taint: true,
+  },
 }
