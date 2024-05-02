7aa6be3962f6de079bacad9ad634a22d8bf475cb
hannahdatum <hannahking@datum.net>
2024-05-02 18:43:05 +0100

    sanity studio setup
    Signed-off-by: hannahdatum <hannahking@datum.net>

---
+++ apps/web/src/sanity.config.ts
@@ -0,0 +1,36 @@
+'use client'
+/**
+ * This config is used to set up Sanity Studio that's mounted on the `app/(sanity)/studio/[[...tool]]/page.tsx` route
+ */
+import { visionTool } from '@sanity/vision'
+import { PluginOptions, defineConfig } from 'sanity'
+import { presentationTool } from 'sanity/presentation'
+import { structureTool } from 'sanity/structure'
+
+import { apiVersion, dataset, projectId, studioUrl } from '@/sanity/lib/api'
+import { locate } from '@/sanity/plugins/locate'
+import { pageStructure, singletonPlugin } from '@/sanity/plugins/settings'
+import { assistWithPresets } from '@/sanity/plugins/assist'
+import author from '@/sanity/schemas/documents/author'
+import post from '@/sanity/schemas/documents/post'
+import settings from '@/sanity/schemas/singletons/settings'
+
+export default defineConfig({
+  basePath: studioUrl,
+  projectId,
+  dataset,
+  schema: {
+    types: [settings, post, author],
+  },
+  plugins: [
+    presentationTool({
+      locate,
+      previewUrl: { previewMode: { enable: '/api/draft' } },
+    }),
+    structureTool({ structure: pageStructure([settings]) }),
+    singletonPlugin([settings.name]),
+    assistWithPresets(),
+    process.env.NODE_ENV === 'development' &&
+      visionTool({ defaultApiVersion: apiVersion }),
+  ].filter(Boolean) as PluginOptions[],
+})
