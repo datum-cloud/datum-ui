bafad6a769f4a2928e966922edcc939a775c5425
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2024-07-08 16:57:21 -0600

    set configurable session config value, default to 1 hour
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- packages/dally/src/index.ts
+++ packages/dally/src/index.ts
@@ -1,3 +1,4 @@
 export const restUrl = process.env.API_REST_URL!
 export const gqlUrl = process.env.NEXT_PUBLIC_API_GQL_URL!
 export const sessionCookieName = process.env.SESSION_COOKIE_NAME
+export const sessionCookieExpiration = process.env.SESSION_COOKIE_EXPIRATION_MINUTES
