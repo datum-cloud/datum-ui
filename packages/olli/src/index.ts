0ed0b4c9c5b2f87d291e1674cd3bc98e8c8a6731
Kevin <kevwilliams@gmail.com>
2026-02-17 06:58:11 -0800

    Fixing imports and Button working

--- packages/olli/src/index.ts
+++ packages/olli/src/index.ts
@@ -1,2 +1,7 @@
+// Components
 export * from "./button";
 export * from "./icons";
+
+// Utilities and hooks
+export * from "./lib";
+export * from "./hooks";
---
+++ packages/olli/src/lib/index.ts
@@ -0,0 +1,1 @@
+export { cn } from "@repo/shadcn/lib/utils";
